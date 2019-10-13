d228fa7bee60c3b418722d060a5848caf93149b5
Raniita <efdezsanchez@gmail.com>
2019-10-13 18:14:50 +0200

    everything working

--- client/src/components/Books.vue
+++ client/src/components/Books.vue
@@ -27,7 +27,7 @@
                             <td>
                                 <div class="btn-group" role="group">
                                     <button type="button" class="btn btn-warning btn-sm" v-b-modal.book-update-modal @click="editBook(book)">Update</button>
-                                    <button type="button" class="btn btn-danger btn-sm">Delete</button>
+                                    <button type="button" class="btn btn-danger btn-sm" @click="onDeleteBook(book)">Delete</button>
                                 </div>
                             </td>
                         </tr>
@@ -206,7 +206,7 @@ export default {
             this.updateBook(payload, this.editForm.id);
         },
         updateBook(payload, bookID) {
-            const path = `http://localhost:5000/book/${bookID}`;
+            const path = `http://localhost:5000/books/${bookID}`;
             axios.put(path, payload)
                 .then(() => {
                     this.getBooks();
@@ -224,6 +224,23 @@ export default {
             this.$refs.editBookModal.hide();
             this.initForm();
             this.getBooks();
+        },
+        removeBook(bookID) {
+            const path = `http://localhost:5000/books/${bookID}`;
+            axios.delete(path)
+                .then(() => {
+                    this.getBooks();
+                    this.message = 'Book removed!';
+                    this.showMessage = true;
+                })
+                .catch((error) => {
+                    // eslint-disable-next-line
+                    console.error(error);
+                    this.getBooks();
+                });
+        },
+        onDeleteBook(book) {
+            this.removeBook(book.id);
         },
     },
     created() {
--- server/app.py
+++ server/app.py
@@ -64,7 +64,7 @@ def all_books():
     return jsonify(response_object)
 
 # edit a book
-@app.route('/book/<book_id>', methods=['PUT'])
+@app.route('/books/<book_id>', methods=['PUT', 'DELETE'])
 def single_book(book_id):
     response_object = {'status': 'success'}
     if request.method == 'PUT':
@@ -77,6 +77,9 @@ def single_book(book_id):
             'read': post_data.get('read')
         })
         response_object['message'] = 'Book updated!'
+    if request.method == 'DELETE':
+        remove_book(book_id)
+        response_object['message'] = 'Book removed!'
     return jsonify(response_object)
 
 if __name__ == '__main__':
