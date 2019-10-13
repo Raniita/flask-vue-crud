f83717878f11de26896551aea58a51fa423c3da8
Raniita <efdezsanchez@gmail.com>
2019-10-13 17:31:39 +0200

    Update modal working

--- client/src/components/Books.vue
+++ client/src/components/Books.vue
@@ -4,6 +4,7 @@
             <div class="col-sm-10">
                 <h1>Books</h1>
                 <hr><br><br>
+                <alert :message="message" v-if="showMessage"></alert>
                 <button type="button" class="btn btn-success btn-sm" v-b-modal.book-modal>Add Book</button>
                 <br><br>
                 <table class="table table-hover">
@@ -25,7 +26,7 @@
                             </td>
                             <td>
                                 <div class="btn-group" role="group">
-                                    <button type="button" class="btn btn-warning btn-sm">Update</button>
+                                    <button type="button" class="btn btn-warning btn-sm" v-b-modal.book-update-modal @click="editBook(book)">Update</button>
                                     <button type="button" class="btn btn-danger btn-sm">Delete</button>
                                 </div>
                             </td>
@@ -69,11 +70,50 @@
             <b-button type="reset" variant="danger">Reset</b-button>
             </b-form>
         </b-modal>
+
+        <b-modal ref="editBookModal"
+                 id="book-update-modal"
+                 title="Update"
+                 hide-footer>
+            <b-form @submit="onSubmitUpdate" @reset="onResetUpdate" class="w-100">
+            <b-form-group id="form-title-edit-group"
+                          label="Title:"
+                          label-for="form-title-edit-input">
+                <b-form-input id="form-title-edit-input"
+                              type="text"
+                              v-model="editForm.title"
+                              required
+                              placeholder="Enter title">
+                </b-form-input>
+            </b-form-group>
+            <b-form-group id="form-author-edit-group"
+                          label="Author:"
+                          label-for="form-author-edit-input">
+                <b-form-input id="form-author-edit-input"
+                              type="text"
+                              v-model="editForm.author"
+                              required
+                              placeholder="Enter author">
+                </b-form-input>
+            </b-form-group>
+            <b-form-group id="form-read-edit-group">
+                <b-form-checkbox-group v-model="editForm.read" id="form-checks">
+                    <b-form-checkbox value="true">Read?</b-form-checkbox> 
+                </b-form-checkbox-group>
+            </b-form-group>
+            <b-button-group>
+                <b-button type="submit" variant="primary">Update</b-button>
+                <b-button type="reset" vairant="danger">Cancel</b-button>
+            </b-button-group>
+            </b-form> 
+        </b-modal>
+
     </div>
 </template>
 
 <script>
 import axios from 'axios';
+import Alert from './Alert.vue'
 
 export default {
     data() {
@@ -84,7 +124,18 @@ export default {
                 author: '',
                 read: [],
             },
+            editForm: {
+                id: '',
+                title: '',
+                author: '',
+                read: [],
+            },
+            message: '',
+            showMessage: false,
         };
+    },
+    components: {
+        alert: Alert,
     },
     methods: {
         getBooks() {
@@ -103,6 +154,8 @@ export default {
             axios.post(path,payload)
                 .then(() => {
                     this.getBooks();
+                    this.message = 'Book added!';
+                    this.showMessage = true;
                 })
                 .catch((error) => {
                     // eslint-disable-next-line
@@ -114,6 +167,10 @@ export default {
             this.addBookForm.title = '';
             this.addBookForm.author = '';
             this.addBookForm.read = [];
+            this.editForm.id = '';
+            this.editForm.title = '';
+            this.editForm.author = '';
+            this.editForm.read = [];
         },
         onSubmit(evt) {
             evt.preventDefault();
@@ -125,13 +182,48 @@ export default {
                 author: this.addBookForm.author,
                 read,
             };
-            this.addBook(payload);
+            this.addBook(payload)
             this.initForm();
         },
-        onReset(evt){
+        onReset(evt) {
             evt.preventDefault();
             this.$refs.addBookModal.hide();
             this.initForm();
+        },
+        editBook(book) {
+            this.editForm = book;
+        },
+        onSubmitUpdate(evt) {
+            evt.preventDefault();
+            this.$refs.editBookModal.hide();
+            let read = false;
+            if (this.editForm.read[0]) read = true;
+            const payload = {
+                title: this.editForm.title,
+                author: this.editForm.author,
+                read,
+            };
+            this.updateBook(payload, this.editForm.id);
+        },
+        updateBook(payload, bookID) {
+            const path = `http://localhost:5000/book/${bookID}`;
+            axios.put(path, payload)
+                .then(() => {
+                    this.getBooks();
+                    this.message = 'Book updated!';
+                    this.showMessage = true;
+                })
+                .catch((error) => {
+                    // eslint-disable-next-line
+                    console.error(error);
+                    this.getBooks();
+                });
+        },
+        onResetUpdate(evt) {
+            evt.preventDefault();
+            this.$refs.editBookModal.hide();
+            this.initForm();
+            this.getBooks();
         },
     },
     created() {
--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import uuid
 
 # configuration
 DEBUG = True
@@ -14,21 +15,31 @@ CORS(app, resource={r'/*':{'origins':'*'}})
 # list of books
 BOOKS = [
     {
+        'id': uuid.uuid4().hex,
         'title': 'On the Road',
         'author': 'Jack Kerouac',
         'read': True
     },
     {
+        'id': uuid.uuid4().hex,
         'title': 'Harry Potter 1',
         'author': 'JK Rowling',
         'read': False
     },
     {
+        'id': uuid.uuid4().hex,
         'title': 'Green Eggs and Ham',
         'author': 'Dr. Seuss',
         'read': True
     }
 ]
+
+def remove_book(book_id):
+    for book in BOOKS:
+        if book['id'] == book_id:
+            BOOKS.remove(book)
+            return True
+    return False
 
 # sanity check route
 @app.route('/ping', methods=['GET'])
@@ -42,6 +53,7 @@ def all_books():
     if request.method == 'POST':
         post_data = request.get_json()
         BOOKS.append({
+            'id': uuid.uuid4().hex,
             'title': post_data.get('title'),
             'author': post_data.get('author'),
             'read': post_data.get('read')
@@ -49,6 +61,22 @@ def all_books():
         response_object['message'] = 'Book added!'
     else:
         response_object['books'] = BOOKS
+    return jsonify(response_object)
+
+# edit a book
+@app.route('/book/<book_id>', methods=['PUT'])
+def single_book(book_id):
+    response_object = {'status': 'success'}
+    if request.method == 'PUT':
+        post_data = request.get_json()
+        remove_book(book_id)
+        BOOKS.append({
+            'id': uuid.uuid4().hex,
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read')
+        })
+        response_object['message'] = 'Book updated!'
     return jsonify(response_object)
 
 if __name__ == '__main__':
