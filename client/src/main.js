d576be20b7c44e6a2923b9d70f06025e5c21901e
Raniita <efdezsanchez@gmail.com>
2019-10-13 15:52:36 +0200

    modal working

--- client/src/main.js
+++ client/src/main.js
@@ -1,7 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import BoostrapVue from 'bootstrap-vue';
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+
+Vue.use(BoostrapVue);
 
 Vue.config.productionTip = false;
 
--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 # configuration
@@ -11,10 +11,45 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app, resource={r'/*':{'origins':'*'}})
 
+# list of books
+BOOKS = [
+    {
+        'title': 'On the Road',
+        'author': 'Jack Kerouac',
+        'read': True
+    },
+    {
+        'title': 'Harry Potter 1',
+        'author': 'JK Rowling',
+        'read': False
+    },
+    {
+        'title': 'Green Eggs and Ham',
+        'author': 'Dr. Seuss',
+        'read': True
+    }
+]
+
 # sanity check route
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
+
+# add or get books
+@app.route('/books', methods=['GET', 'POST'])
+def all_books():
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        BOOKS.append({
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read')
+        })
+        response_object['message'] = 'Book added!'
+    else:
+        response_object['books'] = BOOKS
+    return jsonify(response_object)
 
 if __name__ == '__main__':
     app.run()
